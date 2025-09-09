53a680266212c0c66a3aaf160ace5817ed2d2b14
Andreas Frandsen <buddif@gmail.com>
2025-09-09 21:33:16 +0200

    Remove comment

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-echo "Script started..."
 
 # Fetch the latest SOC from TeslaMate database inside Docker
 SOC=$(docker exec teslamate-database-1 \
       psql -U teslamate teslamate -t -c \
       "SELECT battery_level FROM positions ORDER BY date DESC LIMIT 1;" | xargs)
-
-echo "SOC fetched: $SOC"
 
 source ~/repos/ev-charge-opt/venv/bin/activate
 
